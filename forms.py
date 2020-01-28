69fa177512c4ed536fe57e1b1149036012f80537
aliensmart <kaoua17@gmail.com>
2020-01-28 03:51:55 -0500

    added error message in each require field

--- forms.py
+++ forms.py
@@ -1,8 +1,8 @@
 from wtforms import Form, TextField, TextAreaField, SubmitField, validators, ValidationError
 
 class ContactForm(Form):
-    name = TextField("Name", [validators.Required()])
-    email = TextField("Email", [validators.Required(), validators.Email()])
-    subject = TextField("Subject", [validators.Required()])
-    message = TextAreaField("Message", [validators.Required()])
+    name = TextField("Name", [validators.Required("Please enter your name.")])
+    email = TextField("Email", [validators.Required("Please enter your email."), validators.Email("Please verify your email")])
+    subject = TextField("Subject", [validators.Required("Please enter a Subject.")])
+    message = TextAreaField("Message", [validators.Required("Please enter a message.")])
     submit  = SubmitField("send")
