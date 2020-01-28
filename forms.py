0b138b96bf7b3dce2a55b23c2a6fb1e59bc5f707
aliensmart <kaoua17@gmail.com>
2020-01-28 03:41:25 -0500

    added form validation in forms.py and error message in route.py

--- forms.py
+++ forms.py
@@ -1,8 +1,8 @@
-from wtforms import Form, TextField, TextAreaField, SubmitField
+from wtforms import Form, TextField, TextAreaField, SubmitField, validators, ValidationError
 
 class ContactForm(Form):
-    name = TextField("Name")
-    email = TextField("Email")
-    subject = TextField("Subject")
-    message = TextAreaField("Message")
+    name = TextField("Name", [validators.Required()])
+    email = TextField("Email", [validators.Required(), validators.Email()])
+    subject = TextField("Subject", [validators.Required()])
+    message = TextAreaField("Message", [validators.Required()])
     submit  = SubmitField("send")
--- route.py
+++ route.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, flash
 from forms import ContactForm
 
 app = Flask(__name__)
@@ -8,7 +8,11 @@ app.secret_key = 'development key'
 def contact():
     form = ContactForm()
     if request.method=='POST':
-        return 'Form Posted'
+        if form.validate()== False:
+            flash("all Field are reuired.")
+            return render_template('form.html', form=form)
+        else:
+            return 'Form Posted'
     elif request.method == 'GET':
         return render_template('form.html', form=form)
 
