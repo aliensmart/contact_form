58a51d66d7917b0630ac4e338cb2253193daf71b
aliensmart <kaoua17@gmail.com>
2020-01-28 04:21:15 -0500

    added mail configuration to the route

--- route.py
+++ route.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template, jsonify, request, flash
 from forms import ContactForm
+from flaskext.mail import Mail, Message
 
 app = Flask(__name__)
 app.secret_key = 'development key'
+mail = Mail()
+
+app.config["MAIL_SERVER"] = "smtp.gmail.com" #gmail smtp settings
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = "your_email"
+app.config["MAIL_PASSWORD"] = "your_password"
+
+mail.init_app(app)
+
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
