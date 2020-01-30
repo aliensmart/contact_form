7183ea3b8f1cfecddfc6e7d11cc212f221149343
aliensmart <kaoua17@gmail.com>
2020-01-30 03:36:40 -0500

    secure way to store credentials using os

--- route.py
+++ route.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, jsonify, request, flash
 from forms import ContactForm
 from flaskext.mail import Mail, Message
+import os
+
 
 app = Flask(__name__)
 app.secret_key = 'development key'
@@ -9,8 +11,8 @@ mail = Mail()
 app.config["MAIL_SERVER"] = "smtp.gmail.com" #gmail smtp settings
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USE_SSL"] = True
-app.config["MAIL_USERNAME"] = "your_email"
-app.config["MAIL_PASSWORD"] = "your_password"
+app.config["MAIL_USERNAME"] = os.environ.get('DB_USER')
+app.config["MAIL_PASSWORD"] = os.environ.get('DB_PASS')
 
 mail.init_app(app)
 
@@ -23,7 +25,13 @@ def contact():
             flash("all Field are required.")
             return render_template('form.html', form=form)
         else:
-            return 'Form Posted'
+            msg = Message(form.subject.data, sender='contact@exemple.com', recipients=['your-email@exemple.com'])   
+            msg.body = """
+            From: {} <{}>
+            {}
+            """.format(form.name.data, form.email.data, form.message.data)
+            mail.send(msg)
+            
     elif request.method == 'GET':
         return render_template('form.html', form=form)
 
